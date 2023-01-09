8033f4e5f89ce45d1b937e1025bbcdba43e2c0d9
dcerver <121509667+dcerver@users.noreply.github.com>
2023-01-09 22:04:42 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,27 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    Potato = receivedNumber
+})
+input.onButtonPressed(Button.AB, function () {
+    Potato = randint(10, 20)
+})
+input.onGesture(Gesture.Shake, function () {
+    if (Potato > 0) {
+        radio.sendNumber(Potato)
+        Potato = -1
+    }
+})
+let Potato = 0
+radio.setGroup(1)
+Potato = -1
+basic.forever(function () {
+    if (Potato == 0) {
+        basic.showIcon(IconNames.Skull)
+    }
+    if (Potato > 0) {
+        basic.showIcon(IconNames.Chessboard)
+        Potato += -1
+    }
+    if (Potato < 0) {
+        basic.clearScreen()
+    }
+})
